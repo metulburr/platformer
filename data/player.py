8ff6426613a281b7f6d67dfbc02a3097c3b28ed2
metulburr <micahpage911@yahoo.com>
2014-01-22 09:34:43 -0500

    fixed code

--- data/player.py
+++ data/player.py
@@ -1,4 +1,3 @@
-
 import pygame as pg
 
 class Player:
@@ -15,45 +14,54 @@ class Player:
         self.in_air = False
         self.y_vel = 0
         self.jump_power = 10
-        
+
     def get_event(self, event, keys):
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_SPACE:
                 self.jump()
-                
-    def movementX(self, keys):
+
+    def movementX(self, keys, blocks):
+        frame_speed = 0
         if keys[pg.K_d]:
-            self.rect.x += self.speed
+            frame_speed += self.speed
         if keys[pg.K_a]:
-            self.rect.x -= self.speed
-        
+            frame_speed -= self.speed
+        self.rect.x += frame_speed
+        collide = pg.sprite.spritecollideany(self, blocks)
+        if collide:
+            if frame_speed > 0:
+                self.rect.right = collide.rect.left
+            else:
+                self.rect.left = collide.rect.right
+
     def update(self, keys, blocks):
-        self.rect.clamp_ip(self.screen_rect)
-        self.movementX(keys)
+        self.movementX(keys, blocks)
         self.handle_collision(blocks)
-        
+        self.rect.clamp_ip(self.screen_rect)
+
+    def check_ground(self, blocks):
+        self.rect.move_ip(0,1)
+        if not pg.sprite.spritecollideany(self, blocks):
+            self.in_air = True
+        self.rect.move_ip(0,-1)
+
     def handle_collision(self, blocks):
-        for block in blocks:
-            if not self.rect.colliderect(block) and self.rect.bottom < self.screen_rect.bottom:
-                self.in_air = True
-                break
+        self.check_ground(blocks)
         if self.in_air:
             self.y_vel += self.grav
             self.rect.y += self.y_vel
-            if self.rect.bottom >= self.screen_rect.bottom:
+            collide = pg.sprite.spritecollideany(self, blocks)
+            if collide:
+                if self.y_vel > 0:
+                    self.rect.bottom = collide.rect.top
+                else:
+                    self.rect.top = collide.rect.bottom
                 self.in_air = False
-            for block in blocks:
-                if self.rect.colliderect(block):
-                    if self.rect.bottom >= block.rect.top:
-                        self.in_air = False
-                        break
-        else:
-            self.y_vel = 0
+                self.y_vel = 0
 
-        
     def render(self, screen):
         screen.blit(self.image, self.rect)
-        
+
     def jump(self):
         if not self.in_air:
             self.y_vel = -self.jump_power
--- data/states/game.py
+++ data/states/game.py
@@ -1,26 +1,28 @@
-
 import pygame as pg
 from .. import tools, player, block
 import random
 
 class Game(tools.States):
-    def __init__(self, screen_rect): 
+    def __init__(self, screen_rect):
         tools.States.__init__(self)
         self.screen_rect = screen_rect
         self.score_text, self.score_rect = self.make_text("Game State",
             (255,255,255), (screen_rect.centerx,100), 30)
         self.pause_text, self.pause_rect = self.make_text("PAUSED",
             (255,255,255), screen_rect.center, 50)
-        
+
         #game specific content
         self.bg_color = (0,0,0)
         self.pause = False
-        
+
         self.player = player.Player(self.screen_rect)
         self.blocks = []
         self.blocks.append(block.Block((100,550,50,25)))
         self.blocks.append(block.Block((200,550,50,25)))
-    
+        self.blocks.append(block.Block((325,500,50,25)))
+        self.blocks.append(block.Block((450,450,50,25)))
+        self.blocks.append(block.Block((0,600,800,20)))
+
     def get_event(self, event, keys):
         if event.type == pg.QUIT:
             self.quit = True
@@ -34,7 +36,7 @@ class Game(tools.States):
         self.player.get_event(event, keys)
         #elif event.type == self.background_music.track_end:
         #    self.background_music.track = (self.background_music.track+1) % len(self.background_music.tracks)
-        #    pg.mixer.music.load(self.background_music.tracks[self.background_music.track]) 
+        #    pg.mixer.music.load(self.background_music.tracks[self.background_music.track])
         #    pg.mixer.music.play()
 
     def update(self, now, keys):
@@ -54,27 +56,27 @@ class Game(tools.States):
             block.render(screen)
         if self.pause:
             screen.blit(self.pause_text, self.pause_rect)
-        
+
     def make_text(self,message,color,center,size):
         font = tools.Font.load('arial.ttf', size)
         text = font.render(message,True,color)
         rect = text.get_rect(center=center)
         return text,rect
-        
+
     def adjust_score(self, hit_side):
         if hit_side == -1:
             self.score[1] += 1
         elif hit_side == 1:
             self.score[0] += 1
-            
+
     def const_event(self, keys):
         pass
-            
+
     def cleanup(self):
         self.mouse_cursor.set_as_arrow()
         #pg.mixer.music.stop()
         #self.background_music.setup(self.background_music_volume)
-        
+
     def entry(self):
         pass#self.mouse_cursor.set_as_lowrect()
         #pg.mixer.music.play()
